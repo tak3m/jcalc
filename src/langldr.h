9428536f33d19289e3636760554a0d3b59faf00e
tachikawa20 <73493246+tachikawa20@users.noreply.github.com>
2020-11-16 11:35:49 +0000

    Update langldr.h

--- src/langldr.h
+++ src/langldr.h
@@ -1,1 +1,30 @@
+#include <sstream>
+#include <fstream>
+#include <codecvt>
+
+using namespace std;
+
+string filename = "en_lang.lg"
+wstring inputs[5];
+    
+locale utf8_locale(std::locale(), new gel::stdx::utf8cvt<true>);
+wfstream fs;
+fs.imbue(utf8_locale);
+
+fs.open(filename, wfstream::in);
+if (fs.is_open())
+{
+    int index = 0;
+    while (!fs.eof() && index < 5)
+    {
+        getline(fs, inputs[index]);
+        index++;
+    }
+    fs.close();
+}
+
+wcout << input[0] << endl;
+
+
+
 
